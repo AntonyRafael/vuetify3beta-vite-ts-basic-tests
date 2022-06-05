cc064e261442d161ee8a57b3a40f71764491e4bb
Antony Rafael <depaulacostaantony@gmail.com>
2022-06-05 14:50:34 -0300

    removed vue warn in the test
    Created a rule for custom element vue, to vue read the vuetify components with a custom element

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,21 @@ import { defineConfig } from "vitest/config";
 import vue from "@vitejs/plugin-vue";
 
 // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
+// @ts-ignore
 import vuetify from "vite-plugin-vuetify";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vuetify({ autoImport: true })],
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (tag: string) => tag.includes("*-*") || tag.includes("v-list-item-content"),
+        },
+      },
+    }),
+    vuetify({ autoImport: true }),
+  ],
   test: {
     setupFiles: "vuetify.config.js",
     deps: {
