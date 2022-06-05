e258dba092d0dcc5e63f4d29e42f86c6fcb0cb5a
Antony Rafael <depaulacostaantony@gmail.com>
2022-06-05 11:42:46 -0300

    Added vitest library in the project

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vitest/config";
+import vue from "@vitejs/plugin-vue";
 
 // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
-import vuetify from 'vite-plugin-vuetify'
+import vuetify from "vite-plugin-vuetify";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-		vue(),
-		vuetify({ autoImport: true }),
-]
-})
+  plugins: [vue(), vuetify({ autoImport: true })],
+  test: {
+    setupFiles: "vuetify.config.js",
+    deps: {
+      inline: ["vuetify"],
+    },
+    globals: true,
+  },
+});
