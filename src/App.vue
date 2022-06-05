b708bf170bdbf1fb131cccf71266e582e4b98285
Antony Rafael <depaulacostaantony@gmail.com>
2022-06-05 11:38:43 -0300

    Added basic component

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,71 @@
 <template>
   <v-app>
     <v-main>
-      <h1>{{ msg }}</h1>
+      <v-row class="mt-4 ml-2">
+        <v-col
+          cols="12"
+          md="4"
+          class="pt-0"
+          v-for="item in items"
+          :key="item.id"
+        >
+          <div>
+            <Card
+              :id="item.id"
+              :title="item.title"
+              :content="item.content"
+              :icon="item.icon"
+              :buttonName="item.buttonName"
+              :pathName="item.pathName"
+              :stats="item.stats"
+            />
+          </div>
+        </v-col>
+      </v-row>
     </v-main>
   </v-app>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import Card from "./components/Card.vue";
 
 export default defineComponent({
   name: "App",
   setup() {
     return {
-      msg: "Hello, World !"
+      msg: "Hello, World !",
+      items: [
+        {
+          id: 1,
+          title: "Title Card 1",
+          content: "Content Card 1",
+          icon: "mdi-account-group",
+          stats: 15,
+          buttonName: "Button Test 1",
+          pathName: "/",
+        },
+        {
+          id: 2,
+          title: "Title Card 2",
+          content: "Content Card 2",
+          icon: "mdi-account-group",
+          stats: 25,
+          buttonName: "Button Test 2",
+          pathName: "/",
+        },
+        {
+          id: 3,
+          title: "Title Card 3",
+          content: "Content Card 3",
+          icon: "mdi-account-group",
+          stats: 35,
+          buttonName: "Button Test 3",
+          pathName: "/",
+        },
+      ],
     };
-  }
+  },
+  components: { Card },
 });
 </script>
---
+++ src/components/Card.vue
@@ -0,0 +1,75 @@
+<template>
+  <v-hover v-slot="{ isHovering, props }">
+    <v-card class="pa-2 mb-2" v-bind="props" :elevation="isHovering ? 24 : 8">
+      <v-list-item lines="three">
+        <v-list-item-content>
+          <p class="text-overline mb-4" :title-test="title">
+            {{ title }}
+          </p>
+          <v-list-item-title class="text-h4 mb-1" :stats-test="stats">
+            {{ stats }}
+          </v-list-item-title>
+          <v-list-item-subtitle :content-test="content">
+            {{ content }}
+          </v-list-item-subtitle>
+        </v-list-item-content>
+
+        <v-spacer />
+
+        <template v-slot:append>
+          <v-list-item-avatar end class="mt-4">
+            <v-icon :icon="icon" size="x-large" :iconName-test="icon" />
+          </v-list-item-avatar>
+        </template>
+      </v-list-item>
+
+      <v-card-actions>
+        <v-btn
+          :to="pathName"
+          color="dark"
+          variant="text"
+          tabindex="0"
+          :buttonName-test="buttonName"
+        >
+          {{ buttonName }}
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-hover>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  props: {
+    id: {
+      type: Number,
+      default: 0,
+    },
+    title: {
+      type: String,
+      default: "",
+    },
+    content: {
+      type: String,
+      default: "",
+    },
+    icon: {
+      type: String,
+      default: "",
+    },
+    buttonName: {
+      type: String,
+      default: "",
+    },
+    pathName: {
+      type: String,
+      default: "",
+    },
+    stats: {
+      type: Number,
+      default: 0,
+    },
+  },
+});
+</script>
